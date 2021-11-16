424fc6300aabd369b8fb19c3f3233376e3f29644
Monica D <moniqx4@gmail.com>
2021-11-16 08:41:08 -0700

    Updates to config

--- globalTypes.ts
+++
@@ -1,7 +0,0 @@
-import { Browser, Page, Context } from "playwright";
-declare global {
-  const browser: Browser;
-  const page: Page;
-  const context: Context;
-  const browserName: string;
-}
---
+++ src/globalTypes.ts
@@ -0,0 +1,8 @@
+import { Browser, Page, BrowserContext } from "@playwright/test";
+
+declare global {
+  const browser: Browser;
+  const page: Page;
+  const context: BrowserContext;
+  const browserName: string;
+}
